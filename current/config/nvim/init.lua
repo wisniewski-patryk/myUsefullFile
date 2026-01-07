02061fe33215099e0096e664bcd3b45c36f5ecb2
Patryk Wiśniewski <pwisniewski1991@gmail.com>
2026-01-07 22:02:32 +0100

    nvim config with plugins using vim.pack 0.12+

--- current/config/nvim/init.lua
+++ current/config/nvim/init.lua
@@ -1,11 +1,27 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.wrap = true
+vim.opt.tabstop = 4
+vim.opt.swapfile = false
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 0 -- set to 0 to default to tabstop value
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 
 vim.g.mapleader = " "
+vim.keymap.set('n', '<leader>e', ':Explore<CR>')
 
-vim.opt.expandtab = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 0 -- set to 0 to default to tabstop value
+vim.pack.add({
+    { src = "https://github.com/windwp/nvim-autopairs" },
+    { src = "https://github.com/folke/tokyonight.nvim"}
+})
 
+require("nvim-autopairs")
+
+require("tokyonight").setup({
+    style = "night",
+    transparent = true,
+    terminal_colors = true
+})
+
+vim.cmd([[colorscheme tokyonight]])
